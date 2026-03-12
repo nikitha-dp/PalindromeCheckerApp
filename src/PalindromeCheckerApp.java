f0a6278b1604b4b2d133db025a5547420ea0d33a
nikitha-dp <amyadaksh76@gmail.com>
2026-03-12 14:39:17 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,19 @@
 public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        String str = "madam";
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        if (str.equals(reversed)) {
+            System.out.println("Input String: " + str);
+            System.out.println("Result: It is a Palindrome");
+        } else {
+            System.out.println("Input String: " + str);
+            System.out.println("Result: Not a Palindrome");
+        }
+    }
+
 }
