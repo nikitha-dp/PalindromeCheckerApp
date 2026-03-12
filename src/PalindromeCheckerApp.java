a31880e3746b9f8e5383a252cf23b37404225ddd
nikitha-dp <amyadaksh76@gmail.com>
2026-03-12 14:48:59 +0530

    resolving confilct

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,18 @@
 public class PalindromeCheckerApp {
-  public  static void main(String[] args) {
-        System.out.println("welcome to palindrome checker app");
+        public static void main(String[] args) {
+            String str = "madam";
+            String reversed = "";
+
+            for (int i = str.length() - 1; i >= 0; i--) {
+                reversed += str.charAt(i);
+            }
+
+            if (str.equals(reversed)) {
+                System.out.println("Input String: " + str);
+                System.out.println("Result: It is a Palindrome");
+            } else {
+                System.out.println("Input String: " + str);
+                System.out.println("Result: Not a Palindrome");
+            }
+        }
     }
-}
